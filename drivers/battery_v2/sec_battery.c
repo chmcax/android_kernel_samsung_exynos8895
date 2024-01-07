f724e09d9a31040166837ecbf3a9f5350d7178f9
chmcax <alexchimac@protonmail.com>
2024-01-07 20:11:54 +0200

    exynos8895: fix misleading indentations

--- drivers/battery_v2/sec_battery.c
+++ drivers/battery_v2/sec_battery.c
@@ -5950,8 +5950,8 @@ ssize_t sec_bat_store_attrs(
 					&temp_data[69], &temp_data[70], &temp_data[71],
 					&temp_data[72], &temp_data[73], &temp_data[74],
 					&temp_data[75], &temp_data[76]) <= CISD_DATA_MAX_PER_DAY) {
-					for (i = 0; i < CISD_DATA_MAX_PER_DAY; i++)
-						pcisd->data[i] = 0;
+						for (i = 0; i < CISD_DATA_MAX_PER_DAY; i++)
+							pcisd->data[i] = 0;
 						pcisd->data[CISD_DATA_ALG_INDEX] = battery->pdata->cisd_alg_index;
 						pcisd->data[CISD_DATA_FULL_COUNT] = temp_data[0];
 						pcisd->data[CISD_DATA_CAP_MAX] = temp_data[1];
--- net/core/sock.c
+++ net/core/sock.c
@@ -764,12 +764,12 @@ int sock_setsockopt(struct socket *sock, int level, int optname,
 	if (optname == SO_BINDTODEVICE)
 		return sock_setbindtodevice(sk, optval, optlen);
 
-    /* START_OF_KNOX_NPA */
-    if (optname == SO_SET_DOMAIN_NAME)
-        return sock_set_domain_name(sk, optval, optlen);
-    if (optname == SO_SET_DNS_UID)
-	return sock_set_dns_uid(sk, optval, optlen);
-    /* END_OF_KNOX_NPA */
+	/* START_OF_KNOX_NPA */
+	if (optname == SO_SET_DOMAIN_NAME)
+	        return sock_set_domain_name(sk, optval, optlen);
+	if (optname == SO_SET_DNS_UID)
+		return sock_set_dns_uid(sk, optval, optlen);
+	/* END_OF_KNOX_NPA */
 
 	if (optlen < sizeof(int))
 		return -EINVAL;
